967079492ebc3560b82f7663a52666358d38a99e
Dhara2886 <dhflamingo2886@gmail.com>
2024-06-25 22:25:59 +0100

    My cucumber homework - Dhara

---
+++ src/test/java/com/orangehrmlive/demo/runner/RunCukeSanityTest.java
@@ -0,0 +1,15 @@
+package com.orangehrmlive.demo.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features", // ------ content root path for features directory
+        glue = "com/orangehrmlive/demo/steps", // ---------source root path for steps package
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "json:target/cucumber-reports/cucumber.json"},
+        tags = "@sanity"
+)
+public class RunCukeSanityTest extends AbstractTestNGCucumberTests {
+}
